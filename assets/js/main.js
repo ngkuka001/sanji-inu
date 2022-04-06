e0bb1fe10b3562a6a53828cbc80648ce473585de
vannguyen <ngkuka001@gmail.com>
2022-04-06 22:57:43 +0700

    update script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 window.onload = (event) => {
-  console.log("page is fully loaded");
+  console.log('page is fully loaded');
+  window.addEventListener('scroll', () => {
+    const refs = [...document.querySelectorAll(`[class*="animate__"]`)];
+    const currentScroll = window.pageYOffset;
+    refs.forEach((ele) => {
+      const eleOffset = ele.offsetTop;
+      console.log({ eleOffset });
+      if (currentScroll > eleOffset - screen.height / 2) {
+        ele.classList.add('animate__fadeInUp');
+        ele.classList.remove('animate__fadeOutDown');
+      }
+      if (currentScroll > eleOffset + screen.height / 2) {
+        ele.classList.remove('animate__fadeInUp');
+        ele.classList.add('animate__fadeOutDown');
+      }
+    });
+  });
 };
