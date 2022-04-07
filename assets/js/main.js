430ec3625423b9f0e088a41a90713576553e8a74
ngkuka001 <ngkuka001@gmail.com>
2022-04-07 07:33:31 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 window.onload = (event) => {
-  console.log('page is fully loaded');
-  window.addEventListener('scroll', () => {
+  console.log("page is fully loaded");
+  window.addEventListener("scroll", () => {
     const refs = [...document.querySelectorAll(`[class*="animate__"]`)];
     const currentScroll = window.pageYOffset;
     refs.forEach((ele) => {
       const eleOffset = ele.offsetTop;
       console.log({ eleOffset });
-      if (currentScroll > eleOffset - screen.height / 2) {
-        ele.classList.add('animate__fadeInUp');
-        ele.classList.remove('animate__fadeOutDown');
-      }
-      if (currentScroll > eleOffset + screen.height / 2) {
-        ele.classList.remove('animate__fadeInUp');
-        ele.classList.add('animate__fadeOutDown');
+      if (
+        currentScroll > eleOffset - screen.height / 2 &&
+        currentScroll < eleOffset + screen.height / 2 - 1
+      ) {
+        ele.classList.add("animate__fadeInUp");
+        ele.classList.remove("animate__fadeOutDown");
+      } else {
+        ele.classList.remove("animate__fadeInUp");
+        ele.classList.add("animate__fadeOutDown");
       }
     });
   });
