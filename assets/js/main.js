8d83b0eff0eed4ed45002aef6341ed7c1eaea73d
ngkuka001 <ngkuka001@gmail.com>
2022-04-08 23:07:50 +0700

    update style and content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,18 @@
-// window.onload = (event) => {
-//   console.log("page is fully loaded");
-//   window.addEventListener("scroll", () => {
-//     const refs = [...document.querySelectorAll(`[class*="animate__"]`)];
-//     const currentScroll = window.pageYOffset;
-//     refs.forEach((ele) => {
-//       const eleOffset = ele.offsetTop;
-//       console.log({ eleOffset });
-//       if (
-//         currentScroll > eleOffset - screen.height &&
-//         currentScroll < eleOffset + screen.height / 2 - 100
-//       ) {
-//         ele.classList.add("animate__fadeInUp");
-//         ele.classList.remove("animate__fadeOutDown");
-//       } else {
-//         ele.classList.remove("animate__fadeInUp");
-//         ele.classList.add("animate__fadeOutDown");
-//       }
-//     });
-//   });
-// };
+window.onload = (event) => {
+  window.addEventListener("scroll", () => {
+    const refs = [...document.querySelectorAll(`[class*="animate__"]`)];
+    const currentScroll = window.pageYOffset;
+    refs.forEach((ele) => {
+      const eleOffset = ele.offsetTop;
+      console.log({ eleOffset });
+      if (currentScroll > eleOffset - screen.height / 2) {
+        ele.classList.add("animate__fadeInUp");
+        ele.classList.remove("animate__fadeOut");
+      }
+      //  else {
+      //   ele.classList.remove("animate__fadeInUp");
+      //   ele.classList.add("animate__fadeOut");
+      // }
+    });
+  });
+};
