b03949364dd6d100170c453d12abeefe4f10417b
vannguyen <ngkuka001@gmail.com>
2022-04-09 14:36:42 +0700

    update content and style

--- assets/js/script-sanji.js
+++ assets/js/script-sanji.js
@@ -1,18 +1,40 @@
-window.onload = (event) => {
-  window.addEventListener("scroll", () => {
-    const refs = [...document.querySelectorAll(`[class*="animate__"]`)];
-    const currentScroll = window.pageYOffset;
+window.onload = () => {
+  const refs = [...document.querySelectorAll(`[class*="animate__"]`)];
+  const handleFade = (windowOffset) => {
     refs.forEach((ele) => {
       const eleOffset = ele.offsetTop;
-      console.log({ eleOffset });
-      if (currentScroll > eleOffset - screen.height / 2) {
-        ele.classList.add("animate__fadeInUp");
-        ele.classList.remove("animate__fadeOut");
+      if (windowOffset > eleOffset - screen.height) {
+        ele.classList.add('animate__fadeInUp');
+        ele.classList.remove('animate__fadeOut');
       }
-      //  else {
-      //   ele.classList.remove("animate__fadeInUp");
-      //   ele.classList.add("animate__fadeOut");
-      // }
     });
+  };
+  const initWindow = window.pageYOffset;
+  handleFade(initWindow);
+
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+    handleFade(currentScroll);
+
+    // header sticky
+    const header = document.getElementById('header');
+    currentScroll > 0
+      ? header.classList.add('header-sticky')
+      : header.classList.remove('header-sticky');
   });
+
+  // img-token position
+  const tokenImgs = document.querySelectorAll('.img-token');
+  const handlePositionToken = () => {
+    tokenImgs.forEach((img) => {
+      const nextDiv = img.nextElementSibling;
+      const thisHeight = img.height;
+
+      img.style.top = -(thisHeight / 2) + 'px';
+      nextDiv.style.paddingTop = thisHeight / 2 + 'px';
+    });
+  };
+  handlePositionToken();
+
+  window.addEventListener('resize', handlePositionToken);
 };
